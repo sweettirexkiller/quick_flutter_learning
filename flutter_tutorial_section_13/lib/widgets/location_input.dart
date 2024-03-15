74dd5f2563d0c3a382d20e84c706d6c8adb14c74
Stanisław Jankiewicz <stanislawjankiewicz@Stanisaws-MacBook-Pro.local>
2024-03-15 14:41:57 +0100

    location works

--- flutter_tutorial_section_13/lib/widgets/location_input.dart
+++ flutter_tutorial_section_13/lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -9,11 +10,62 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  Location? _pickedLocation;
+  var _isGettingLocation = false;
 
+  void _getCurrentLocation() async {
+    Location location = new Location();
 
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+
+    print(locationData.latitude);
+    print(locationData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Center(
+      child: Text('No Location Chosen',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onBackground,
+        ),),
+    );
+
+
+    if(_isGettingLocation){
+      previewContent = const Center(child: CircularProgressIndicator(),);
+    }
+
+
     return Column(
       children: [
         Container(
@@ -23,18 +75,12 @@ class _LocationInputState extends State<LocationInput> {
           decoration: BoxDecoration(
             border: Border.all(width: 1, color: Theme.of(context).colorScheme.primary.withOpacity(0.2)),
           ),
-          child: Center(
-            child: Text('No Location Chosen',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),),
-          ),
+          child: previewContent,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            TextButton.icon(icon: const Icon(Icons.location_on), label: const Text('Choose Location'),onPressed:  () {}),
+            TextButton.icon(icon: const Icon(Icons.location_on), label: const Text('Get Current Location'),onPressed: _getCurrentLocation),
             TextButton.icon(
               onPressed: () {},
               icon: const Icon(CupertinoIcons.map),
